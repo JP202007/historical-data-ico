7c068f5d3e643ce1e6af63b5bcc0c00af4ada427
JP202007 <68064142+JP202007@users.noreply.github.com>
2020-07-15 16:25:13 +0200

    Add files via upload

---
+++ 2016/merge.py
@@ -0,0 +1,11 @@
+import os
+import pandas as pd
+import glob
+
+csv_list = glob.glob('*.csv')
+print(u'find %s CSV files'% len(csv_list))
+for i in csv_list:
+    fr = open(i,'rb').read()
+    with open('result-ico2016.csv','ab') as f:
+        f.write(fr)
+print(u'merged OK！')
